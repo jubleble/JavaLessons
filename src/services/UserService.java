e7a301dbaa5697beb9c3b3f5d8ebd16ba551ae08
Pawel Kubiak <pawel.kubiak@ormlondon.com>
2018-11-27 21:41:34 +0100

    Implement structure of UserService

---
+++ src/services/UserService.java
@@ -0,0 +1,21 @@
+package services;
+
+import models.User;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by pawelk on 27/11/2018.
+ */
+public class UserService {
+    public void sendMessage(User user) {
+        throw new NotImplementedException();
+    }
+
+    public User[] getUsersWhoSubscribeEmailChannel(User[] allUsers) {
+        throw new NotImplementedException();
+    }
+
+    public User[] getUsersWhoHaveMoreLoyaltyPointsThan(User[] allUsers, int minimumExpectedLoyaltyPoints) {
+        throw new NotImplementedException();
+    }
+}
