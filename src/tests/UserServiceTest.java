763f4951f875679c390d628a4ed1e44074957982
Pawel Kubiak <pawel.kubiak@ormlondon.com>
2018-11-27 22:02:35 +0100

    Unit Test Cases created

---
+++ src/tests/UserServiceTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by pawelk on 27/11/2018.
+ */
+public class UserServiceTest {
+    /*
+        // todo
+        1. WHEN allUsersIsEmpty THEN empty array will be returned
+        2. WHEN allUsersIsNotEmpty AND allUserSaidYesToEmail THEN allUsersArrayShouldBeReturned
+        3. WHEN allUsersIsNotEmpty AND allUserSaidNoToEmail THEN empty array will be returned
+        4. WHEN allUsersIsNotEmpty AND someOfUsersSaidYesToEmail THEN array with only users who said 'yes' to Email chanel will be returned
+     */
+}
