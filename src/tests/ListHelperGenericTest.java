a11628f8a70131869d13db30b5afab4c2ba7e48c
gandwhite <tomasz.patronik@gmail.com>
2018-12-09 21:55:28 +0100

    Implemented methods & tests: findItem, addItem, removeItem, swapItems Overridden equals() method for User class

--- src/tests/ListHelperGenericTest.java
+++ src/tests/ListHelperGenericTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import models.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import utils.ListHelperGeneric;
@@ -11,7 +12,7 @@ public class ListHelperGenericTest {
         getMinValue
         1. when arrayList is empty return NULL
         2. when arrayList is not empty return min value
-     */
+    */
     @Test public void getMinValue_WhenArrayListIsEmpty_ThenReturnNull(){
         //Arrange
         ListHelperGeneric<Integer> listHelperGeneric = new ListHelperGeneric<>();
@@ -45,7 +46,7 @@ public class ListHelperGenericTest {
         getMaxValue
         1. when arrayList is empty return NULL
         2. when arrayList is not empty return max value
-     */
+    */
     @Test public void getMaxValue_WhenArrayListIsEmpty_ThenReturnNull(){
         //Arrange
         ListHelperGeneric<Integer> listHelperGeneric = new ListHelperGeneric<>();
@@ -75,4 +76,255 @@ public class ListHelperGenericTest {
         Assertions.assertEquals(expectedValue,resultValue);
     }
 
+    /*
+        findItem
+        1. when list is empty then return false
+        2. when list is not empty and element exists return true
+        3. when list is not empty and element does not exist return false
+    */
+    @Test public void findItem_WhenListIsEmpty_ThenReturnFalse(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>();
+        User inputItem = new User("a@b.c","a","b",true,true,true,true,"876","ad");
+        boolean expectedValue = false;
+
+        //Act
+        boolean result = listHelper.findItem(userList,inputItem);
+
+        //Assert
+        Assertions.assertEquals(expectedValue,result);
+    }
+
+    @Test public void findItem_WhenListIsNotEmptyAndItemIsNotPresent_ThenReturnFalse(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("a1@b.c","a","b",true,true,true,true,"876","ad"));
+        userList.add(new User("a2@b.c","a","b",true,true,true,true,"876","ad"));
+
+        User inputItem = new User("a@b.c","a","b",true,true,true,true,"876","ad");
+
+        boolean expectedValue = false;
+
+        //Act
+        boolean result = listHelper.findItem(userList,inputItem);
+
+        //Assert
+        Assertions.assertEquals(expectedValue,result);
+    }
+
+    @Test public void findItem_WhenListIsNotEmptyAndItemIsPresent_ThenReturnTrue(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+
+        User inputItem = new User("0","a","b",true,true,true,true,"876","ad");
+
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("0","a","b",true,true,true,true,"876","ad"));
+        userList.add(new User("a2@b.c","a","b",true,true,true,true,"876","ad"));
+
+        boolean expectedValue = true;
+
+        //Act
+        boolean result = listHelper.findItem(userList,inputItem);
+
+        //Assert
+        Assertions.assertEquals(expectedValue,result);
+    }
+
+    /*
+        addItem
+        1. when list is empty return list with length = 1 and with item at 0 position
+        2. when list is not empty return list with length = length+1 and item at last position
+    */
+    @Test public void addItem_WhenListIsEmpty_ThenReturnListWithItemAtLastPosition(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>(0);
+        User inputItem = new User("0","a","b",true,true,true,true,"876","ad");
+        int expectedLength = 1;
+        User expectedItem = new User("0","a","b",true,true,true,true,"876","ad");
+
+        //Act
+        ArrayList<User> resultList = listHelper.addItem(userList,inputItem);
+
+        //Assert
+        Assertions.assertEquals(expectedLength,resultList.size());
+        Assertions.assertEquals(expectedItem,resultList.get(0));
+    }
+
+    @Test public void addItem_WhenListIsNotEmpty_ThenReturnListWithItemAtLastPosition(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("a2@b.c","a","b",true,true,true,true,"876","ad"));
+        userList.add(new User("adf2@b.c","asdf","bsdf",true,true,true,true,"876","ad"));
+
+        User inputItem = new User("111","1","1",true,true,true,true,"876","ad");
+        int expectedLength = 3;
+        User expectedItem = new User("111","1","1",true,true,true,true,"876","ad");
+
+        //Act
+        ArrayList<User> resultList = listHelper.addItem(userList,inputItem);
+
+        //Assert
+        Assertions.assertEquals(expectedLength,resultList.size());
+        Assertions.assertEquals(expectedItem,resultList.get(resultList.size()-1));
+    }
+
+    /*
+        removeItem
+        1. when item is not present in list then return the same list
+        2. when item is present in list, remove item and return list smaller by one
+        3. when list is empty then return null
+    */
+    @Test public void removeItem_WhenItemIsNotInTheList_ThenReturnTheSameList(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("a2@b.c","a","b",true,true,true,true,"876","ad"));
+        userList.add(new User("adf2@b.c","asdf","bsdf",true,true,true,true,"876","ad"));
+        int expectedLength = 2;
+        User itemToBeRemoved = new User("222","asdf","bsdf",true,true,true,true,"876","ad");
+
+        //Act
+        ArrayList<User> resultList = listHelper.removeItem(userList,itemToBeRemoved);
+
+        //Assert
+        Assertions.assertEquals(expectedLength,resultList.size());
+        for (int i = 0; i < userList.size(); i++){
+            Assertions.assertEquals(userList.get(i),resultList.get(i));
+        }
+    }
+
+    @Test public void removeItem_WhenItemIsInTheList_ThenRemoveItemAndReturnSmallerList(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("a2@b.c","a","b",true,true,true,true,"876","ad"));
+        userList.add(new User("222","asdf","bsdf",true,true,true,true,"876","ad"));
+        userList.add(new User("adf2@b.c","asdf","bsdf",true,true,true,true,"876","ad"));
+
+        User itemToBeRemoved = new User("222","asdf","bsdf",true,true,true,true,"876","ad");
+
+        int expectedLength = 2;
+        User zeroIndexResult = new User("a2@b.c","a","b",true,true,true,true,"876","ad");
+        User firstIndexResult = new User("adf2@b.c","asdf","bsdf",true,true,true,true,"876","ad");
+
+        //Act
+        ArrayList<User> resultList = listHelper.removeItem(userList,itemToBeRemoved);
+
+        //Assert
+        Assertions.assertEquals(expectedLength,resultList.size());
+        Assertions.assertEquals(zeroIndexResult,resultList.get(0));
+        Assertions.assertEquals(firstIndexResult,resultList.get(1));
+    }
+
+    @Test public void removeItem_WhenListIsEmpty_ThenReturnNull(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>();
+        User itemToBeRemoved = new User("222","asdf","bsdf",true,true,true,true,"876","ad");
+        ArrayList<User> expectedResult = null;
+
+        //Act
+        ArrayList<User> resultList = listHelper.removeItem(userList,itemToBeRemoved);
+
+        //Assert
+        Assertions.assertEquals(expectedResult,resultList);
+    }
+
+    /*
+        swapItems
+        1. When list is empty Then return NULL
+        2. When list is not empty and both items are present Then return list with swapped items
+        3. When list is not empty and first item is not present Then return original list
+        4. When list is not empty and second item is not present Then return original list
+    */
+    @Test public void swapItems_WhenListIsEmpty_ThenReturnNull(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>();
+        User firstItemToBeSwapped = new User("222","asdf","bsdf",true,true,true,true,"876","ad");
+        User secondItemToBeSwapped = new User("111","111","111",true,true,true,true,"876","ad");
+        ArrayList<User> expectedResult = null;
+
+        //Act
+        ArrayList<User> resultList = listHelper.swapItems(userList,firstItemToBeSwapped,secondItemToBeSwapped);
+
+        //Assert
+        Assertions.assertEquals(expectedResult,resultList);
+    }
+
+    @Test
+    public void swapItems_WhenListIsNotEmptyAndBothItemsArePresent_ThenReturnListWithSwappedItems(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("222","222","222",true,true,true,true,"876","ad"));
+        userList.add(new User("111","111","111",true,true,true,true,"876","ad"));
+        userList.add(new User("333","333","333",true,true,true,true,"876","ad"));
+
+        User firstItemToBeSwapped = new User("222","222","222",true,true,true,true,"876","ad");
+        User secondItemToBeSwapped = new User("333","333","333",true,true,true,true,"876","ad");
+
+        //Act
+        ArrayList<User> resultList = listHelper.swapItems(userList,firstItemToBeSwapped,secondItemToBeSwapped);
+
+        //Assert
+        Assertions.assertEquals(firstItemToBeSwapped,resultList.get(2));
+        Assertions.assertEquals(secondItemToBeSwapped,resultList.get(0));
+    }
+
+    @Test
+    public void swapItems_WhenListIsNotEmptyAndFirstItemIsNotPresent_ThenReturnOriginalList(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("111","111","111",true,true,true,true,"876","ad"));
+        userList.add(new User("444","444","444",true,true,true,true,"876","ad"));
+        userList.add(new User("333","333","333",true,true,true,true,"876","ad"));
+
+        User firstItemToBeSwapped = new User("222","222","222",true,true,true,true,"876","ad");
+        User secondItemToBeSwapped = new User("333","333","333",true,true,true,true,"876","ad");
+
+        User firstResultItem = new User("111","111","111",true,true,true,true,"876","ad");
+        User secondResultItem = new User("444","444","444",true,true,true,true,"876","ad");
+        User thirdResultItem = new User("333","333","333",true,true,true,true,"876","ad");
+
+        //Act
+        ArrayList<User> resultList = listHelper.swapItems(userList,firstItemToBeSwapped,secondItemToBeSwapped);
+
+        //Assert
+        Assertions.assertEquals(firstResultItem,resultList.get(0));
+        Assertions.assertEquals(secondResultItem,resultList.get(1));
+        Assertions.assertEquals(thirdResultItem,resultList.get(2));
+    }
+
+    @Test
+    public void swapItems_WhenListIsNotEmptyAndSecondItemIsNotPresent_ThenReturnOriginalList(){
+        //Arrange
+        ListHelperGeneric<User> listHelper = new ListHelperGeneric<>();
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("111","111","111",true,true,true,true,"876","ad"));
+        userList.add(new User("222","222","222",true,true,true,true,"876","ad"));
+        userList.add(new User("555","555","555",true,true,true,true,"876","ad"));
+
+        User firstItemToBeSwapped = new User("222","222","222",true,true,true,true,"876","ad");
+        User secondItemToBeSwapped = new User("333","333","333",true,true,true,true,"876","ad");
+
+        User firstResultItem = new User("111","111","111",true,true,true,true,"876","ad");
+        User secondResultItem = new User("222","222","222",true,true,true,true,"876","ad");
+        User thirdResultItem = new User("555","555","555",true,true,true,true,"876","ad");
+
+        //Act
+        ArrayList<User> resultList = listHelper.swapItems(userList,firstItemToBeSwapped,secondItemToBeSwapped);
+
+        //Assert
+        Assertions.assertEquals(firstResultItem,resultList.get(0));
+        Assertions.assertEquals(secondResultItem,resultList.get(1));
+        Assertions.assertEquals(thirdResultItem,resultList.get(2));
+    }
 }
--- src/utils/ListHelperGeneric.java
+++ src/utils/ListHelperGeneric.java
@@ -2,17 +2,15 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created by gandwhite on 05/12/2018.
- * todo
  *  IMPLEMENTED get min value from list (public T getMinValue(T[] arraylist))
  *  IMPLEMENTED get max value from list (public T getMaxValue(T[] arraylist))
- *  find item from list (public bool findItem(T[] arraylist, T item))
- *  add item to list (public T[] addItem(T[] arraylist, T item))
- *  remove item from list (public T[] removeItem(T[] arraylist, T item))
- *  swap items in list (public T[] swapItems(T[] arraylist, T firstItem, T secondItem))
+ *  IMPLEMENTED find item from list (public bool findItem(T[] arraylist, T item))
+ *  IMPLEMENTED add item to list (public T[] addItem(T[] arraylist, T item))
+ *  IMPLEMENTED remove item from list (public T[] removeItem(T[] arraylist, T item))
+ *  IMPLEMENTED swap items in list (public T[] swapItems(T[] arraylist, T firstItem, T secondItem))
  */
 
 public class ListHelperGeneric<T>{
@@ -45,5 +43,47 @@ public class ListHelperGeneric<T>{
                 temp = next;
         }
         return temp;
+    }
+
+    public boolean findItem(ArrayList<T> arrayList, T item){
+        return arrayList.contains(item);
+    }
+
+    public ArrayList<T> addItem(ArrayList<T> arrayList, T item){
+        arrayList.add(item);
+        return arrayList;
+    }
+
+    public ArrayList<T> removeItem(ArrayList<T> arrayList, T item){
+        if(arrayList.isEmpty()) {
+            return null;
+        }
+        if(!arrayList.contains(item)) {
+            return arrayList;
+        }
+        arrayList.remove(item);
+        return arrayList;
+    }
+
+    public ArrayList<T> swapItems(ArrayList<T> arrayList, T firstItem, T secondItem){
+        if(arrayList.isEmpty()){
+            return null;
+        }
+        if(!arrayList.contains(firstItem)){
+            return arrayList;
+        }
+        if(!arrayList.contains(secondItem)){
+            return arrayList;
+        }
+
+        int firstItemIndex = arrayList.indexOf(firstItem);
+        int secondItemIndex = arrayList.indexOf(secondItem);
+
+        //T temp = firstItem;
+        T temp = arrayList.get(arrayList.indexOf(firstItem));
+        arrayList.set(firstItemIndex,secondItem);
+        arrayList.set(secondItemIndex,temp);
+
+        return arrayList;
     }
 }
