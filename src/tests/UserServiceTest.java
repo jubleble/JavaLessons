24e702fb6a50208ee6d3d33f72e6595208982263
Lenovo <emildrzewudzki@gmail.com>
2019-01-03 15:46:18 +0100

    Add test findUserByFirstNameAndLastName_WhenArrayIsEmpty_ThenShouldReturnEmptyArray

--- src/tests/UserServiceTest.java
+++ src/tests/UserServiceTest.java
@@ -1,6 +1,10 @@
 package tests;
 
+import exceptions.UserNotFoundException;
+import models.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import services.UserService;
 
 /**
  * Created by pawelk on 27/11/2018.
@@ -13,4 +17,25 @@ public class UserServiceTest {
         3. WHEN allUsersIsNotEmpty AND allUserSaidNoToEmail THEN empty array will be returned
         4. WHEN allUsersIsNotEmpty AND someOfUsersSaidYesToEmail THEN array with only users who said 'yes' to Email chanel will be returned
      */
+
+    //1. users array is empty then empty array
+    @Test
+    public void findUserByFirstNameAndLastName_WhenArrayIsEmpty_ThenShouldReturnEmptyArray() {
+        // Arrange
+        UserService userService = new UserService();
+        User[] EmptyArray = new User[0];
+        String firstName = "Test3";
+        String surname  = "Test4";
+
+        // Act
+        User [] result = new User[0];
+        try {
+            result = userService.findUser(EmptyArray,firstName, surname);
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Assert
+        Assertions.assertEquals(EmptyArray, result);
+    }
 }
