acbf328434b66d7ea038b63a71e255644d8d1a80
Lenovo <emildrzewudzki@gmail.com>
2019-01-12 15:14:46 +0100

    Implemented Test updateUserAddress_WhenAddressIsNew_ThenUpdateNewAddress

--- src/tests/UserServiceTest.java
+++ src/tests/UserServiceTest.java
@@ -314,6 +314,7 @@ class UserServiceTest {
         Assertions.assertTrue(exceptionBoolean);
         Assertions.assertNotEquals(newAddress,user.getAddress());
     }
+
     @Test
     void updateUserAddress_WhenAddressIsSameLikeOld_ThenWrongAddressException() {
         // Arrange
@@ -335,4 +336,24 @@ class UserServiceTest {
         Assertions.assertEquals(newAddress,user.getAddress());
     }
 
+    @Test
+    void updateUserAddress_WhenAddressIsNew_ThenUpdateNewAddress() {
+        // Arrange
+        UserService userService = new UserService();
+        User user = new User ("test@test.com","test","test1",false,true,true,true,"12345678","test1");
+        String newAddress = "test2";
+        boolean exceptionBoolean = false;
+
+        // Act
+        try {
+            userService.updateUserAddress(user,newAddress);
+        } catch (WrongAddressException e) {
+            exceptionBoolean = true;
+            System.out.println(e.toString());
+        }
+
+        // Assert
+        Assertions.assertFalse(exceptionBoolean);
+        Assertions.assertEquals(newAddress,user.getAddress());
+    }
 }
